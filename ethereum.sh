053a121a8dd50adcb6d42fd92a74519f3d4d5af2
EasySwap <142835812+EziSwap@users.noreply.github.com>
2024-04-27 22:57:20 +0300

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-d34b4f18-a10d-40ae-b30e-5da6e4d2c3be/"
 
 OUTPUT_DIR="logs"
 
